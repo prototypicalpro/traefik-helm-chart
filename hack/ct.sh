2de6b22aa08dbc3a2291985d44c629f028294823
Noah Koontz <noah@koontzs.com>
2024-03-30 18:58:34 +0000

    update gh-pages

--- hack/ct.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git config --global --add safe.directory /charts
-
-ct $1 --config=.github/chart-testing.yaml --charts traefik/
--- hack/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/helm unittest --color ./traefik;
